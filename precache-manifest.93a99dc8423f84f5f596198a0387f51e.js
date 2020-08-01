e04b031ad802e0cc1b7c89ed9c5d3eebb05d9ac3
Vee Upatising <veeupatising@gmail.com>
2020-08-01 16:37:15 +0800

    Slider Update
    Slider now performs inference on model

---
+++ precache-manifest.93a99dc8423f84f5f596198a0387f51e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1f9c189000fd5b09fa31c99b469afbe8",
+    "url": "/Anime-DCGAN/index.html"
+  },
+  {
+    "revision": "6c035422fd30ade83948",
+    "url": "/Anime-DCGAN/static/css/2.bb4ba17c.chunk.css"
+  },
+  {
+    "revision": "26015f8a42ac6cec4835",
+    "url": "/Anime-DCGAN/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "6c035422fd30ade83948",
+    "url": "/Anime-DCGAN/static/js/2.d88e802d.chunk.js"
+  },
+  {
+    "revision": "347f7e9daf456fa73233f94e5baa0752",
+    "url": "/Anime-DCGAN/static/js/2.d88e802d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "26015f8a42ac6cec4835",
+    "url": "/Anime-DCGAN/static/js/main.7f02bef0.chunk.js"
+  },
+  {
+    "revision": "7a2e918f20bb1c21053a",
+    "url": "/Anime-DCGAN/static/js/runtime-main.9645cd5a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Anime-DCGAN/static/media/logo.5d5d9eef.svg"
+  }
+]);
